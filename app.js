51315cf1447a6889f3e1f9d0f2c735e7eccd1c6e
Alexander <gadget343@gmail.com>
2015-09-10 15:06:49 +0300

    git commit

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+/* global __dirname */
+
 var express = require('express'),
     fs = require('fs'),
     passport = require('./server/config/passport'),
     app = express();
 
 fs.readdirSync(__dirname + '/server/routes').forEach(function(file) {
-        console.log(file)
-        var name = file.substr(0, file.indexOf('.'));
-        require('./server/routes/' + name)(app);
-      });
+    var name = file.substr(0, file.indexOf('.'));
+    require('./server/routes/' + name)(app);
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/server/views');
+app.use(express.static('public'));
 
 app.listen(3002);
